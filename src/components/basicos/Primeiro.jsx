6434241e4bffb5e853a817688b7ad18ba33e4dd5
Miguel Cabral <miguelccarvalho1@gmail.com>
2022-03-03 15:29:05 -0300

    update files

--- src/components/basicos/Primeiro.jsx
+++ src/components/basicos/Primeiro.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
-
 export default function Primeiro() {
+
         const MSG_BOAS_VINDAS = "Seja bem vindo(a) ao nosso aplicativo!"
 
-        return (<div>
+        return (
+        <div>
                         <h2>Primeiro Componente</h2>
                         <p>{MSG_BOAS_VINDAS}</p>
                 </div>
--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import './index.css'
 import ReactDom from 'react-dom'
 import React from 'react'
 
-import Primeiro from './components/basicos/Primeiro.js'
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 // const generalErrorString = <strong>Ocorreu um erro inesperado!</strong>
 // const VALOR_PI = 3.14
 
 ReactDom.render(
         
-        <div>
+        <div id = "app">
                 <Primeiro></Primeiro>
+                <ComParametro 
+                        titulo = "Situação do aluno" 
+                        aluno = "Pedro Silva" 
+                        nota = {8.3} />
+
+                <ComParametro 
+                        titulo = "Situação da aluna" 
+                        aluno = "Maria Joaquina" 
+                        nota = {9.9} />
+                
         </div>,
         document.getElementById('root')
 )
